6438aab4cde4a962b7aa7f46600fe998c6b85875
AbanobRafik <abanob_rafik@yahoo.com>
2023-03-22 11:44:04 -0700

    24hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+nclude"main.h"
+
+/**
+ *  * jack_bauer- prints every minute of the day
+ */
+
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0 ; hr < 24 ; hr++)
+	{
+		for (min = 0 ; min < 60 ; min++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			putchar(':');
+			putchar((minute / 10) + '0');
+			putchar((minute % 10) + '0');
+			putchar('\n');
+		}
+	}
+}				
