55f22677635cdb042292d0d0c323eb67d0649e00
AbanobRafik <abanob_rafik@yahoo.com>
2023-03-28 10:45:27 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,32 +9,19 @@
  */
 void puts_half(char *str)
 {
-	int len, i;
-	int half_len = 0;
+	int l, n;
 
-	while (str[len] != '\0')
-	{
-		len++;
-	}
+	for (l = 0; str[l] != '\0'; ++l)
+		;
 
-	half_len = len / 2;
-
-	if (len % 2 == 0)
+	if (l % 2 == 0)
 	{
-		for (i = half_len ; i < len ; i++)
-		{
-			putchar(str[i]);
-		}
-		putchar('\n');
-	}
-	else
+		for (n = l / 2; str[n] != '\0'; ++n)
+			putchar(str[n]);
+	} else
 	{
-		half_len = (len - 1) / 2;
-		for (i = half_len ; i < len ; i++)
-		{
-			putchar(str[i]);
-		}
-		putchar('\n');
+		for (n = ((l - 1) / 2) + 1; str[n] != '\0'; ++n)
+			putchar(str[n]);
 	}
+	putchar('\n');
 }
-
