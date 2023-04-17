7df838860c134d59cc16af91703434bae09ea0d3
AbanobRafik <abanob_rafik@yahoo.com>
2023-04-17 16:18:11 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ *  * struct dog - a structure representing a dog
+ *   * @name: the name of the dog (char *)
+ *    * @age: the age of the dog (float)
+ *     * @owner: the name of the owner of the dog (char *)
+ *      *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
