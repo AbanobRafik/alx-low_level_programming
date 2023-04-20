38de266590301ae2279321426fe5a52ad032da94
AbanobRafik <abanob_rafik@yahoo.com>
2023-04-20 15:58:24 -0700

    c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -29,6 +29,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (i < n - 1 && separator)
 			printf("%s", separator);
 	}
-		printf("\n");
 		va_end(args);
+		printf("\n");
 }
